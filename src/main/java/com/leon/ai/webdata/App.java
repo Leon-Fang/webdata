43087111ba36b8006513a30137a5cc760a92b85c
Leon-Fang <13022143580@163.com>
2020-07-25 17:14:04 +0800

    start project webdata

---
+++ src/main/java/com/leon/ai/webdata/App.java
@@ -0,0 +1,13 @@
+package com.leon.ai.webdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
