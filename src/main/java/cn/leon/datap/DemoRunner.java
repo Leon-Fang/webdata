b84fcf87f83834d1900672e15c8972ddec1f6a3a
LeonFang <leon.fang@sap.com>
2020-07-31 12:50:01 +0800

    add cnweather

--- src/main/java/cn/leon/datap/DemoRunner.java
+++ src/main/java/cn/leon/datap/DemoRunner.java
@@ -1,13 +1,14 @@
 package cn.leon.datap;
 
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.FilePipeline;
 
 public class DemoRunner {
 	public static void main(String[] args) {
-		Spider.create(new MyPageProcessor())
-		      .addUrl("https://baidu.com")
-		      //.addPipeline(new ConsolePipeline())
-		      .thread(5)
+		Spider.create(new MyPageProcessor())			  
+			  .addUrl("http://www.weather.com.cn/weather/101220601.shtml")  // anqing weather.
+		      .addPipeline(new FilePipeline("./data/shanghai"))
+		      .thread(1)
 		      .run();
 
 	}
--- src/main/java/cn/leon/datap/MyPageProcessor.java
+++ src/main/java/cn/leon/datap/MyPageProcessor.java
@@ -16,8 +16,25 @@ public class MyPageProcessor implements PageProcessor{
 	@Override
 	public void process(Page page) {
 		// TODO Auto-generated method stub
-		page.putField("title", page.getHtml().links().all());
-	
+		//page.addTargetRequests(page.getHtml().links().all());
+		page.putField("cities", page.getHtml().xpath("/html/body/div[3]/div/div[2]/div[4]/div[2]/dl[1]/dd/a/text()").all());
+		
+		page.putField("links", page.getHtml().xpath("/html/body/div[3]/div/div[2]/div[4]/div[2]/dl[1]/dd/a").links().all());
+		
+		page.putField("title", page.getHtml().xpath("//*[@id='7d']/ul/li/h1/text()").all());
+		
+		page.putField("weather", page.getHtml().xpath("//*[@id='7d']/ul/li/p[@class='wea']/text()").all());
+		
+		page.putField("Maxtem", page.getHtml().xpath("//*[@id='7d']/ul/li/p[@class='tem']/span/text()").all());
+		
+		page.putField("Mintem", page.getHtml().xpath("//*[@id='7d']/ul/li/p[@class='tem']/i/text()").all());
+		
+		page.putField("wind", page.getHtml().xpath("//*[@id=\"7d\"]/ul/li/p[3]/em").$("span", "title").all());
+		
+		page.putField("windlevel", page.getHtml().xpath("//*[@id=\"7d\"]/ul/li/p[3]/i/text()").all());
+		
+		//page.putField("links", page.getHtml().xpath("//div[@name='area_channel_div_abc']/span[1]/a").links().all());
+		
 	}
 
 	@Override
