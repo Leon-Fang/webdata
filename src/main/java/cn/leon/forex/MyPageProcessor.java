c10d446742bc399b558d743bf6d7b56cb5876f54
LeonFang <leon.fang@sap.com>
2020-08-08 17:00:38 +0800

    half work

--- src/main/java/cn/leon/forex/MyPageProcessor.java
+++ src/main/java/cn/leon/forex/MyPageProcessor.java
@@ -1,6 +1,13 @@
 package cn.leon.forex;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
 
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
@@ -23,7 +30,7 @@ public class MyPageProcessor implements PageProcessor{
 		String mainCountryRate_link = "http://data.eastmoney.com/cjsj/globalRate.html";
 		String globalEcoData_link = "http://data.eastmoney.com/cjsj/foreign_0_2.html";
 		List<String> datalinks;
-		if(page.getUrl().toString().equalsIgnoreCase("http://forex.eastmoney.com")) {
+	    if(page.getUrl().toString().equalsIgnoreCase("http://forex.eastmoney.com")) {
 			//get 新闻链接, 全球汇率 全球经济日历 主要国家利率 全球经济数据
 			news_link = page.getHtml().xpath("//*[@id='newsDD1']/div[2]/a").links().get();
 			datalinks = page.getHtml().xpath("//*[@id=\"newsDD1\"]/div[1]/div[1]/ul/li[2]").links().all();
@@ -48,23 +55,79 @@ public class MyPageProcessor implements PageProcessor{
 	private void PasrseNewsPage(Page page) {		
 	    if(page.getUrl().toString().contains("/a/cwhdd")){
 			List<String> DetailNew_links = page.getHtml().xpath("//*[@id='newsListContent']").links().all();
-			List<String> PageIndex_links = page.getHtml().xpath("//*[@id='pagerNoDiv']/a").links().all();       
-			page.addTargetRequests(DetailNew_links);
-			page.putField("DetailNews_link: ", DetailNew_links);
-			page.putField("PageIndex_links", PageIndex_links);	
-			if(!PageIndex_links.isEmpty()) {
-				page.addTargetRequests(PageIndex_links);
+			List<String> PageIndex_links = page.getHtml().xpath("//*[@id='pagerNoDiv']/a").links().all();  
+			
+			List<String> DetailNewlinks = checkIfDoneThisPage(DetailNew_links);
+			
+			if(DetailNewlinks.size()>0) {				
+			    page.addTargetRequests(DetailNewlinks);
+				page.putField("DetailNews_link", DetailNewlinks);
+				page.putField("PageIndex_links", PageIndex_links);
+			    if(!PageIndex_links.isEmpty()) {
+					page.addTargetRequests(PageIndex_links);
+				}
 			}
-			page.addTargetRequests(DetailNew_links);
 	    }else if(page.getUrl().toString().contains("/a/2")) {
 			parseDetailNews(page);	
 		} 	
 	}
 
+	private List<String> checkIfDoneThisPage(List<String> detailNew_links){
+		List<String> result = new ArrayList<String>();
+		List<String> rs = getLinkFromDB();
+		for(String string : detailNew_links) {
+			if(rs.contains(string)) {
+				System.out.println("this page already done before: "+string);
+			}else {
+			 result.add(string);
+			}			
+		}
+		return result;	
+	}
+
+	private List<String> getLinkFromDB(){
+		String selectString = "select link from Links order by UpdateTime desc LIMIT 40";
+		String linkString;
+		ResultSet rs = getDataFromDB(selectString);
+		List<String> resultList = new ArrayList<String>();
+		try {
+			while (rs.next()) {
+				linkString=rs.getString("link");
+			    resultList.add(linkString);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultList;
+	}
+
+	private ResultSet getDataFromDB(String selectString) {
+		Connection connection;
+		Statement stmt;
+		ResultSet rs;
+		try { 
+			Class.forName("org.sqlite.JDBC");   // ---...JDBC upper case!!!
+			System.out.println("loaded class");
+			connection = DriverManager.getConnection("jdbc:sqlite:FXdata.db");
+			System.out.println("get connection");
+			stmt = connection.createStatement();
+			System.out.println("start exectuing sql");
+			rs = stmt.executeQuery(selectString);
+			System.out.println(" sql end!");
+			stmt.close();
+			connection.close();
+			return rs;
+		} catch (Exception e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		    System.exit(0);
+		    return null;
+	   }		
+	}
+
 	private void parseDetailNews(Page page) {
-		page.putField("article title: ", page.getHtml().xpath("/html/body/div[1]/div/div[3]/div[1]/div[2]/div[1]/h1/text()").get());
-		page.putField("article time: ", page.getHtml().xpath("/html/body/div[1]/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/text()").get());
-		page.putField("article contetn: ", page.getHtml().xpath("//*[@id=\"ContentBody\"]/p/allText()").all());
+		page.putField("article_title", page.getHtml().xpath("/html/body/div[1]/div/div[3]/div[1]/div[2]/div[1]/h1/text()").get());
+		page.putField("article_time", page.getHtml().xpath("/html/body/div[1]/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/text()").get());
+		page.putField("article_content", page.getHtml().xpath("//*[@id=\"ContentBody\"]/p/allText()").all());
 	}
 
 	@Override
--- src/main/java/cn/leon/forex/MyPipeline.java
+++ src/main/java/cn/leon/forex/MyPipeline.java
@@ -1,15 +1,159 @@
 package cn.leon.forex;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
 
-public class MyPipeline implements Pipeline{
 
+public class MyPipeline implements Pipeline{
+	private String NewsSqlString = "INSERT INTO fxNews (articleTitle,articletime,articlecontent) VALUES ";
+	private String NewsUpdate;
+//	private String values1;
+//	private static String sqlString;
 	@Override
 	public void process(ResultItems resultItems, Task task) {
+		saveAllLinks(resultItems);
+		saveFXNews2db(resultItems);
+		saveglobalForex2db(resultItems);
+		saveglobalEcoCalendar2db(resultItems);
+		savemainCountryRate2db(resultItems);
+		saveglobalEcoData2db(resultItems);
+	}	
+	
+	private void saveAllLinks(ResultItems resultItems) {
+		List<String> linksList = resultItems.get("DetailNews_link");
+		if(!(linksList==null)) {
+			if(NotExistIdDB(linksList)) {
+				String LinksSqlString = "INSERT INTO Links (link) VALUES ";
+				for(String string : linksList ) {
+					LinksSqlString = LinksSqlString + "(\""
+									 + string + "\"),";							 	
+				}
+				LinksSqlString = LinksSqlString.substring(0, LinksSqlString.length()-1)+";";
+				saveToDb(LinksSqlString);	
+			}
+			
+		}
 		
+	}
+
+	private boolean NotExistIdDB(List<String> linksList) {
 		
+		return false;
+	}
+
+	private boolean saveglobalEcoData2db(ResultItems resultItems) {
+	   Boolean result = false;
+	   
+       return result;
+       
+	}
+
+	private void savemainCountryRate2db(ResultItems resultItems) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void saveglobalEcoCalendar2db(ResultItems resultItems) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void saveglobalForex2db(ResultItems resultItems) {
+		
+	}
+
+	private void saveFXNews2db(ResultItems resultItems) {
+		System.out.println("get page: " + resultItems.getRequest().getUrl());
+		if(!(resultItems.get("article_title")==null)) {
+		
+			String li0 = resultItems.get("article_title").toString();
+			System.out.println("li0 " + li0);
+			String li1 = resultItems.get("article_time").toString();
+			System.out.println("li1 " + li1);
+			String li2 = resultItems.get("article_content").toString();
+			System.out.println("li2 " + li2);
+			NewsSqlString =String.format("INSERT INTO fxNews (articleTitle,articletime,articlecontent) VALUES (\"%s\",\"%s\",\"%s\")",li0,li1,li2);
+			saveToDb(NewsSqlString);	
+/*
+			values1 =  " (\""
+						  + li0 + "\" ,\""
+						  + li1 + "\" ,\""
+						  + li2 + "\" ,"
+						  + ") " + ",";
+*/
+			}
+ //           sqlString = sqlString.substring(0, sqlString.length()-1)+";";
+			System.out.println("sql : " + NewsSqlString);			
+//			saveToDb(NewSqlString);			
+	}
+
+	private void saveToDb(String sql) {
+		Connection connection;
+		Statement stmt;
+		try { 
+			Class.forName("org.sqlite.JDBC");   // ---...JDBC upper case!!!
+			System.out.println("loaded class");
+			connection = DriverManager.getConnection("jdbc:sqlite:FXdata.db");
+			System.out.println("get connection");
+			stmt = connection.createStatement();
+			System.out.println("start exectuing sql");
+			stmt.executeUpdate(sql);
+			System.out.println(" sql end!");
+			stmt.close();
+			connection.close();
+			
+		} catch (Exception e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		    System.exit(0);
+	}
+	}
+	
+	private List<String> getLinkFromDB(){
+		String selectString = "select link from Links";
+		String linkString;
+		ResultSet rs = getDataFromDB(selectString);
+		List<String> resultList = new ArrayList<String>();
+		try {
+			while (rs.next()) {
+				linkString=rs.getString("link");
+			    resultList.add(linkString);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultList;
+	}
+
+	private ResultSet getDataFromDB(String selectString) {
+		Connection connection;
+		Statement stmt;
+		ResultSet rs;
+		try { 
+			Class.forName("org.sqlite.JDBC");   // ---...JDBC upper case!!!
+			System.out.println("loaded class");
+			connection = DriverManager.getConnection("jdbc:sqlite:FXdata.db");
+			System.out.println("get connection");
+			stmt = connection.createStatement();
+			System.out.println("start exectuing sql");
+			rs = stmt.executeQuery(selectString);
+			System.out.println(" sql end!");
+			stmt.close();
+			connection.close();
+			return rs;
+		} catch (Exception e) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		    System.exit(0);
+		    return null;
+	   }		
 	}
 
 }
