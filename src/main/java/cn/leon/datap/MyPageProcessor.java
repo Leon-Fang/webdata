5c4b6c3a06651d003988a9236b69e3e7b7e4c7e2
LeonFang <leon.fang@sap.com>
2020-08-15 14:52:36 +0800

    fianl version

--- src/main/java/cn/leon/datap/MyPageProcessor.java
+++ src/main/java/cn/leon/datap/MyPageProcessor.java
@@ -43,7 +43,6 @@ public class MyPageProcessor implements PageProcessor{
 	}
 
 	
-	@SuppressWarnings("null")
 	private List<String> getlinks(List<String> linksList) {
 		List<String> linkStrings = new ArrayList<String>();
 		for(String link : linksList) {
