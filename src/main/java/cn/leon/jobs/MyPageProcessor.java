98444a91bcac4c87c5e0757ca6c216032aa9c4ca
LeonFang <leon.fang@sap.com>
2020-08-18 14:45:13 +0800

    get django trainings

--- src/main/java/cn/leon/jobs/MyPageProcessor.java
+++ src/main/java/cn/leon/jobs/MyPageProcessor.java
@@ -25,110 +25,17 @@ public class MyPageProcessor implements PageProcessor{
 	
 	@Override
 	public void process(Page page) {
-		/*
-		 * String news_link = "http://forex.eastmoney.com/a/cwhdd.html"; String
-		 * globalEcoCalendar_link = "http://forex.eastmoney.com/FC.html";
-		 * 
-		 * if(page.getUrl().toString().equalsIgnoreCase("http://forex.eastmoney.com")) {
-		 * news_link =
-		 * page.getHtml().xpath("//*[@id='newsDD1']/div[2]/a").links().get();
-		 * page.putField("news_link", news_link); page.addTargetRequest(news_link);
-		 * 
-		 * page.addTargetRequest(globalEcoCalendar_link); }else
-		 * if(page.getUrl().toString().contains("/a/")) {
-		 * System.out.println("start to get news~!!!"); PasrseNewsPage(page); }else
-		 * if(page.getUrl().toString().contains("/FC.html")) {
-		 * logger.info("start to get important data canlendar"); parseEcoCalendar(page);
-		 * }
-		 */
-		page.putField("linnks: ", page.getHtml().links().all());
-		
+		page.addTargetRequests(page.getHtml().xpath("//*[@id='sidebar']").links().all());
+		parseWebPage(page);
 	}
 	
-	private void parseEcoCalendar(Page page) {
-		page.putField("calendarData", page.getHtml().xpath("//*[@id='tbody']/tr/td/text()").all());
-		
-	}
-
-	private void PasrseNewsPage(Page page) {		
-	    if(page.getUrl().toString().contains("/a/cwhdd")){
-			List<String> DetailNew_links = page.getHtml().xpath("//*[@id='newsListContent']").links().all();
-			List<String> PageIndex_links = page.getHtml().xpath("//*[@id='pagerNoDiv']/a").links().all();  
-			
-			List<String> ModifiedNewlinks = checkIfDoneThisPage(DetailNew_links);
-			System.out.println("DetailNew_links"+DetailNew_links);
-			System.out.println("ModifiedNewlinks:"+ModifiedNewlinks);
-			if(ModifiedNewlinks.size()>0) {				
-			    page.addTargetRequests(ModifiedNewlinks);
-				page.putField("DetailNews_link", ModifiedNewlinks);
-				page.putField("PageIndex_links", PageIndex_links);
-			    if(!PageIndex_links.isEmpty()) {
-					page.addTargetRequests(PageIndex_links);
-				}
-			}
-	    }else if(page.getUrl().toString().contains("/a/2")) {
-			parseDetailNews(page);	
-		} 	
-	}
-
-	private List<String> checkIfDoneThisPage(List<String> detailNew_links){
-		List<String> result = new ArrayList<String>();
-		List<String> rs = getLinkFromDB();
-		System.out.println("get from db links:"+rs);
-		for(String string : detailNew_links) {
-			if(rs.contains(string)) {
-				System.out.println("this page already done before: "+string);
-			}else {
-			 result.add(string);
-			}			
-		}
-		return result;	
-	}
-
-	private List<String> getLinkFromDB() {
-		String selectString = "select DISTINCT link from Links order by UpdateTime LIMIT 40";
-		Connection connection = null;
-		Statement stmt = null;
-		ResultSet rs;
-		String linkString;
-		List<String> resultList = new ArrayList<String>();
-		System.out.println("sql is: "+selectString);
-		try { 
-			Class.forName("org.sqlite.JDBC");   // ---...JDBC upper case!!!
-			System.out.println("loaded class");
-			connection = DriverManager.getConnection("jdbc:sqlite:FXdata.db");
-			System.out.println("get connection");
-			stmt = connection.createStatement();
-			System.out.println("start exectuing sql");
-			rs = stmt.executeQuery(selectString);
-			System.out.println(" sql end!");			
-			while (rs.next()) {
-				linkString=rs.getString("link");
-				resultList.add(linkString);
-			}
-		} catch (Exception e) {
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		    System.exit(0);
-	   }finally {
-		   try {
-			   stmt.close();
-				connection.close();
-		   } catch (Exception e2) {
-			System.out.println("db failed!!!");
-		  }
-	   }	
-	 return	resultList;
-	}
-
-	private void parseDetailNews(Page page) {
-		page.putField("article_title", page.getHtml().xpath("/html/body/div[1]/div/div[3]/div[1]/div[2]/div[1]/h1/text()").get());
-		page.putField("article_time", page.getHtml().xpath("/html/body/div[1]/div/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/text()").get());
-		page.putField("article_content", page.getHtml().xpath("//*[@id=\"ContentBody\"]/p/allText()").all());
-	}
-
+    public void parseWebPage(Page page){
+    	page.putField("Title", page.getHtml().xpath("/html/body/div[3]/div/div[2]/h1/text()").get());
+		page.putField("Contents", page.getHtml().xpath("/html/body/div[3]/div/div[2]").all());
+	} 
+    
 	@Override
-	public Site getSite() {
-		
+	public Site getSite() {		
 		return site;
 	}
 }
--- src/main/java/cn/leon/jobs/MyPipeline.java
+++ src/main/java/cn/leon/jobs/MyPipeline.java
@@ -1,211 +1,50 @@
 package cn.leon.jobs;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import us.codecraft.webmagic.Page;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
 
 
 public class MyPipeline implements Pipeline{
-	private String NewsSqlString = "INSERT INTO fxNews (articleTitle,articletime,articlecontent) VALUES ";
+	static int timeString = 0;
 
 	@Override
 	public void process(ResultItems resultItems, Task task) {
-		System.out.println("current page is: " + resultItems.getRequest().getUrl());
-		saveAllLinks(resultItems);
-		saveFXNews2db(resultItems);
-		saveglobalEcoCalendar2db(resultItems);
-	}	
-	
-	private void saveAllLinks(ResultItems resultItems) {
-		List<String> linksList = resultItems.get("DetailNews_link");
-		if(!(linksList==null)) {
-			List<String> linksindb = getLinkFromDB();  /////--
-			System.out.println("linksList from page are:"+linksList);
-			System.out.println("End to get links from Links and linksindb are: "+linksindb);  //----
-			List<String> linksNotindb = LinksNotExistInDb(linksList,linksindb); //--
-			System.out.println("linksNotindb"+linksNotindb);
-			String LinksSqlString = "INSERT INTO Links (link) VALUES ";
-			for(String string : linksNotindb ) {
-				LinksSqlString = LinksSqlString + "(\""
-									 + string + "\"),";							 	
-			}
-			LinksSqlString = LinksSqlString.substring(0, LinksSqlString.length()-1)+";";
-			saveToDb(LinksSqlString);				
-		}
-		
-	}
-
-	private List<String> LinksNotExistInDb(List<String> linksList, List<String> linksindb) {
-		List<String> result = new ArrayList<String>();
-		for(String s:linksList) {
-			if(!linksindb.contains(s)) {
-				result.add(s);
-			}
-		}
-		return result;
-	}
-	
-	private void saveglobalEcoCalendar2db(ResultItems resultItems){		
-		System.out.println("get page: " + resultItems.getRequest().getUrl());
-		if(!(resultItems.get("calendarData")==null)) {
-			String sqlgetDBdat = "select * from ecocalendar order by num LIMIT 200";
-			List<String> wantedStrings = new ArrayList<String>();
-			wantedStrings.add("publishDate");
-			wantedStrings.add("Event");
-			wantedStrings.add("Country");
-			List<String> resultSet =  getDataFromDB2(sqlgetDBdat,wantedStrings);
-			System.out.println("list get from db: " + resultSet);
-			List<String> dataList = resultItems.get("calendarData");			
-			List<String> dataList2 = new ArrayList<String>();
-			int counter = 0;
-			String sqlvalues="";
-			String sql = "";
-			String sqlo = "INSERT INTO ecocalendar (Id,publishDate,publishTime,Country,Event,ReportPeriod,PublishData,PredictedData,PreValue,importance,tendency) VALUES ";			
-			for(int i = 0; i<dataList.size(); i++) {
-				if(counter<11) {
-					dataList2.add(dataList.get(i));
-				}else {
-					counter = 0;
-					for(int j=0; j<dataList2.size();j++) {
-					   sqlvalues += "\""+dataList2.get(j)+"\",";
-					}
-					sqlvalues = sqlvalues.substring(0, sqlvalues.length()-1);
-					sql = sqlo+"("+sqlvalues+")";
-					if(!AlreadyInDb(dataList2,resultSet)) {
-						saveToDb(sql);						
-					}
-					dataList2.clear();
-					sqlvalues="";
-					sql="";
-					dataList2.add(dataList.get(i));
-				}				
-				counter++;
-			}
-		}
-	}
-
-
-	private boolean AlreadyInDb(List<String> dataList2, List<String> dataFromDB) {
-		boolean result = false;
-		for (String s :dataList2 ) {
-			if(dataFromDB.contains(s) & dataFromDB.contains(s) & dataFromDB.contains(s)) {
-				result = true;
-				break;
-			}
-		}
-		return result;
-	}
-
-	private void saveFXNews2db(ResultItems resultItems) {
-		System.out.println("get page: " + resultItems.getRequest().getUrl());
-		if(!(resultItems.get("article_title")==null)) {
-		
-			String li0 = resultItems.get("article_title").toString();
-			System.out.println("li0 " + li0);
-			String li1 = resultItems.get("article_time").toString();
-			System.out.println("li1 " + li1);
-			String li2 = resultItems.get("article_content").toString();
-			System.out.println("li2 " + li2);
-			NewsSqlString =String.format("INSERT INTO fxNews (articleTitle,articletime,articlecontent) VALUES (\"%s\",\"%s\",\"%s\")",li0,li1,li2);
-			saveToDb(NewsSqlString);	
-			}
- //           sqlString = sqlString.substring(0, sqlString.length()-1)+";";
-			System.out.println("sql : " + NewsSqlString);		
-	}
-
-	private void saveToDb(String sql) {
-		System.out.println("sql to save to db is "+sql);
-		Connection connection;
-		Statement stmt;
-		try { 
-			Class.forName("org.sqlite.JDBC");   // ---...JDBC upper case!!!
-			connection = DriverManager.getConnection("jdbc:sqlite:FXdata.db");
-			stmt = connection.createStatement();
-			System.out.println("start exectuing sql");
-			stmt.executeUpdate(sql);
-			System.out.println(" sql end!");
-			stmt.close();
-			connection.close();			
-		} catch (Exception e) {
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		    System.exit(0);
-	}
-	}
-	
-	private List<String> getLinkFromDB() {
-//		String selectString = "select DISTINCT link from Links order by UpdateTime LIMIT 40";
-		String selectString = "select DISTINCT link from Links";
-		Connection connection = null;
-		Statement stmt = null;
-		ResultSet rs;
-		String linkString;
-		List<String> resultList = new ArrayList<String>();
-		System.out.println("sql is: "+selectString);
-		try { 
-			Class.forName("org.sqlite.JDBC");   // ---...JDBC upper case!!!
-			System.out.println("loaded class");
-			connection = DriverManager.getConnection("jdbc:sqlite:FXdata.db");
-			System.out.println("get connection");
-			stmt = connection.createStatement();
-			System.out.println("start exectuing sql");
-			rs = stmt.executeQuery(selectString);
-			System.out.println(" sql end!");			
-			while (rs.next()) {
-				linkString=rs.getString("link");
-				resultList.add(linkString);
-			}
-		} catch (Exception e) {
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		    System.exit(0);
-	   }finally {
-		   try {
-			   stmt.close();
-				connection.close();
-		   } catch (Exception e2) {
-			System.out.println("db failed!!!");
-		  }
-	   }	
-	 return	resultList;
-	}
-
-	private List<String> getDataFromDB2(String selectString, List<String> wantedStrings) {
-		Connection connection;
-		Statement stmt;
-		ResultSet rs;
-		List<String> resultList = new ArrayList<String>();
-		System.out.println("sql for query is : " + selectString);
-		try { 
-			Class.forName("org.sqlite.JDBC");   // ---...JDBC upper case!!!
-			connection = DriverManager.getConnection("jdbc:sqlite:FXdata.db");
-			stmt = connection.createStatement();
-			System.out.println("start exectuing sql");
-			rs = stmt.executeQuery(selectString);
-			try {
-				while (rs.next()) {
-					for(String s:wantedStrings) {
-						resultList.add(rs.getString(s));
-					}
+		String content = resultItems.get("Contents").toString();
+		String filename = resultItems.get("Title");
+		System.out.println("Title: "+ filename);
+//		System.out.println("content: "+content);
+		if (content != null) {	
+			String pathString = "./Blogs_Contents/formatFiles/"+filename+".html";
+//			System.out.println("pathString: "+pathString);
+			File file = new File(pathString);		
+			if (!file.exists()) {	
+				File dir = new File(file.getParent());
+				dir.mkdirs();
+				try {
+					file.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-			} catch (SQLException e) {
-				e.printStackTrace();
 			}
-			System.out.println("sql end!");
-			stmt.close();
-			connection.close();
-		} catch (Exception e) {
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	   }
-		return resultList;		
-	}
-	
-
+			FileWriter fileWriter = null;		
+			try {
+				fileWriter = new FileWriter(pathString);
+				fileWriter.write(content);	
+			} catch (IOException e) {			
+				e.printStackTrace();
+			}finally {
+				try {
+					fileWriter.flush();
+					fileWriter.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			timeString++;
+		}		
+	}	
 }
